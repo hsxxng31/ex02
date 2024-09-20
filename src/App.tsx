ed8c8326dd632efb78cdaf4f73f5eee857a86382
Choi, Hyeseung <chs390731@gmail.com>
2024-09-21 02:32:10 +0900

    src/App.css, src/App.tsx 내용 변경 p.10

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  let persons = [{ name: '홍길동', age: 16 }, 
-                 { name: '임꺽정', age: 19 },
-                 { name: '전우치', age: 20 }];
-
-  let trlist = persons.map(person => 
-                <tr><td>{person.name}</td><td>{person.age}</td></tr>
+  return (
+    <div>
+      <p className="blue">hello world</p>
+      <p className="green">안녕하세요</p>
+    </div>
   );
-  return <table>
-          <tr><td>이름</td><td>나이</td></tr>
-          {trlist}
-         </table>
 }
 
 export default App;
-
