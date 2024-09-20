143e0fb0534e4bbf45cd259a9ffa2f23c0e4a4c9
Choi, Hyeseung <chs390731@gmail.com>
2024-09-21 01:45:12 +0900

    tr, table 문법 간결하게 변경

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,13 @@ function App() {
                  { name: '임꺽정', age: 19 },
                  { name: '전우치', age: 20 }];
 
-  let td1 = <td>이름</td>;
-  let td2 = <td>나이</td>;
-  let tr = React.createElement("tr", null, td1, td2);
+  let tr = <tr><td>나이</td><td>이름</td></tr>;
   let trlist = [tr];
   for (let person of persons) {
-    td1 = <td>{ person.name }</td>
-    td2 = <td>{ person.age }</td>
-    tr = React.createElement("tr", null, td1, td2);
+    tr = <tr><td>{person.name}</td><td>{person.age}</td></tr>
     trlist.push(tr);
   }
-  let table = React.createElement("table", null, trlist);
+  let table = <table>{trlist}</table>;
   return table;
 }
 
