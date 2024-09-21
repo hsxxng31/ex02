88066541610be6d66721d2600dde47833b27071e
Choi, Hyeseung <chs390731@gmail.com>
2024-09-21 16:05:09 +0900

    구구단 연습문제 TimesTable 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import MyTable from "./MyTable";
+import TimesTable from "./TimesTable";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <MyTable />
       <hr />
       <MyTable />
+      <TimesTable />
     </div>
   );
 }
---
+++ src/TimesTable.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./MyTable.css";
+
+function TimesTable() {
+  let num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  // 구구단 테이블 생성
+  let timesTable = num.map((a, index) => (
+    <tr className="grayBg">
+      {num.map((b) => (
+        <td>
+          {b} * {a} = {a * b}
+        </td>
+      ))}
+    </tr>
+  ));
+
+  return (
+    <table className="MyTable">
+      <tbody>{timesTable}</tbody>
+    </table>
+  );
+}
+
+export default TimesTable;
